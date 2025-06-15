9ef7f9432cbced249f0917caafc83594ccf7a000
RohitChougale <rohitchougale0609@gmail.com>
2025-06-15 18:39:37 +0530

    CounterOrder printing amended

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.79qmhtbf9po"
+    "revision": "0.4d1v5vdk4hg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
