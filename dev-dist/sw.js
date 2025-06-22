b1836b42b1a1a408c5f1215f9cfd12dec2944aa2
RohitChougale <rohitchougale0609@gmail.com>
2025-06-22 16:57:06 +0530

    Counter Anylatics graph and functinality implemented

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1oq3000d2ao"
+    "revision": "0.eal6fu49fhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
