5dfb68a325b236c9823e0f499701fa4e08232797
RohitChougale <rohitchougale0609@gmail.com>
2025-07-12 17:38:03 +0530

    Place order delay increased to prevent from garbage printing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.k9jlpemanog"
+    "revision": "0.6u23c03a4j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
